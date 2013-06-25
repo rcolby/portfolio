381054ca129a1b6d0046999502b6d585fd713da0
Rob Colby <rcolby@me.com>
2013-06-25 15:34:37 -0700

    Adding jQuery magic for scrolling effect.

---
+++ js/portfolio.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $window = $(window);
+
+  $('section[data-type="background"]').each(function() {
+    var $bgobj = $(this);
+    $(window).scroll(function() {
+      var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+      var coords = '50% '+ yPos + 'px';
+      $bgobj.css({ backgroundPosition: coords });
+    });
+  });
+});
+
+// IE Fix
+document.createElement("article");
+document.createElement("section");
